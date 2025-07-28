a3bac3e75a36cdf21ecffe792b7657a4a3287366
Nyde2283 <edwynchev@gmail.com>
2025-07-28 18:52:33 +0200

    Remove old fish functions

--- private_dot_config/private_fish/functions/op.fish
+++
@@ -1,3 +0,0 @@
-function op
-    open $argv > /dev/null 2>&1
-end
--- private_dot_config/private_fish/functions/reload.fish
+++
@@ -1,6 +0,0 @@
-function reload
-    if status is-interactive
-        # Commands to run in interactive sessions can go here
-        oh-my-posh init fish --config "~/.config/assets/unicorn.omp.json" | source
-    end
-end
