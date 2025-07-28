b2171cb5be9e9c4e0015c0c5f58af7a287f098e7
Nyde2283 <edwynchev@gmail.com>
2025-07-28 18:38:01 +0200

    Add exit keybinding

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,6 +10,7 @@ if status is-interactive
     alias lg "git log --oneline"
     bind \er 'echo; echo; commandline -f repaint'
     bind \cl 'reset ; commandline -f repaint'
+    bind \cd 'exit'
     bind ctrl-alt-q 'reset ; source ~/.config/fish/config.fish ; commandline -f repaint ; echo "Prosternez vous devant le grand poulpy, mascotte de la MP2I" | cowsay -f ~/.config/.assets/octopus.cow | lolcat'
     bind \cq 'reset ; source ~/.config/fish/config.fish ; commandline -f repaint'
     bind \ea 'fish_list_all_current_token'
